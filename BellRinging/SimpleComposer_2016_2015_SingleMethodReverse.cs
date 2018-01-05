1e89c3e376b40dcb87164a9287a5a4cc1b0fe789
rbso <robert_oakeshott@hotmail.com>
2018-01-05 20:25:01 +0000

    >130/15

--- BellRinging/SimpleComposer_2016_2015_SingleMethodReverse.cs
+++ BellRinging/SimpleComposer_2016_2015_SingleMethodReverse.cs
@@ -25,7 +25,7 @@ namespace BellRinging
         MethodLibrary lib = new MethodLibrary();
         //int l = 7 * 32 * 9 +1;// -10 * 32;
         int l = 2019;
-        bestTotalMusic = 140; //225 * l / 2500; // start looking for some music
+        bestTotalMusic = 160; //225 * l / 2500; // start looking for some music
         var backstrokeStart = true;
         //backstrokeStart = false;
         Problem p = new Problem()
@@ -38,7 +38,7 @@ namespace BellRinging
             MaxLength = l, //10*32*7,
             Reverse = false,
             MusicDelta = 40,
-            MaxCalls = 11,
+            MaxCalls = 15,
             MaxSingles = 999,
             MaxBobs=999
         };
